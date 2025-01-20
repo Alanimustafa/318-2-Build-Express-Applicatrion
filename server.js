cc6e7c7e069e06de6da5652fb162982c09efe618
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-20 17:20:46 -0600

    Update the about view, the application routes have been added

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Reuireing the express server
 const express = require('express');
 
+// Crating the body code parser
+const bodyParser = require("body-parser");
+
 // Intialize the Web Application Server with Express.
 const app = express();
 
@@ -13,12 +16,14 @@ app.set('view engine', 'pug');
 // Set the views
 app.set("views", "./views");
 
-// Staring app server 
-app.listen(PORT, (req, res) => {
-    console.log(`Server started listening to the PORT # ${PORT}`);
-})
 
 
+// Middleware for parsing form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// Assigning the static files to the app.
 app.use (express.static("public"));
 
 
@@ -29,6 +34,30 @@ app.use (express.static("public"));
 // --------------------------------------- Routes -----------------
 
 app.get("/", (req, res) => {
-    console.log("This is the main Route");
-    res.send('This is the main Route');
+    console.log('Home');
+    res.render("home", { title: "Home", active: "Home" });
+})
+
+app.get("/home", (req, res) => {
+    console.log('Home');
+    res.render("home", { title: "Home", active: "Home" });
+})
+
+app.get("/about", (req, res) => {
+    console.log('About');
+    res.render("about", { title: "About", active: "About" });
+  });
+  
+  app.get("/contactus", (req, res) => {
+    console.log('Contact Us');
+    res.render("contactUs", { title: "Contact Us", active: "Contact Us" });
+  });
+
+
+
+
+// --------------------------- Starting the Server -------------------------
+// Staring the application server 
+app.listen(PORT, (req, res) => {
+    console.log(`Server started listening to the PORT # ${PORT}`);
 })
